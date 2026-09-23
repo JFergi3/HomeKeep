58047542fa723cb33db9ec742e74e7ec16ba0c82
Jake Ferguson <jferguson15@my.wctc.edu>
2026-09-22 19:50:15 -0500

    Added Attributes

--- Models/MaintenanceTask.cs
+++ Models/MaintenanceTask.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
 namespace HomeKeep.Models;
 
 public class MaintenanceTask
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(60, MinimumLength = 2)]
     public string Name { get; set; } = "";
+
+    [StringLength(500)]
     public string Description { get; set; } = "";
+
+    [Required]
+    [StringLength(60)]
     public string Room { get; set; } = "";
+
+    [Display(Name = "Frequency (days)")]
+    [Range(1, 3650)]
     public int FrequencyDays { get; set; }
     public bool IsCompleted { get; set; }
 }
